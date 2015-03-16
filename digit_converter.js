09a4a4effab35f37fd674d139a668b6d16017a18
Payer Ahammed <payer.cse@gmail.com>
2015-03-17 00:35:42 +0600

    Update digit_converter.js

--- digit_converter.js
+++ digit_converter.js
@@ -4,3 +4,9 @@
             return String.fromCharCode(d.charCodeAt(0) + 2486);
         });
     }
+// English to Arabic
+    function digits_en2ar(number) {
+        return number.toString().replace(/\d/g, function(d){
+            return String.fromCharCode(d.charCodeAt(0) + 1584);
+        });
+    }
