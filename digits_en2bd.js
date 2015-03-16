ad22bca2aad8483ad82d7c946e578e3af42d2ed2
Payer Ahammed <payer.cse@gmail.com>
2015-03-16 23:36:36 +0600

    Update digits_en2bd.js

--- digits_en2bd.js
+++ digits_en2bd.js
@@ -1,1 +1,6 @@
-
+// English digit to Bangla 
+    function digits_en2bd(number) {
+        return number.toString().replace(/\d/g, function(d){
+            return String.fromCharCode(d.charCodeAt(0) + 2486);
+        });
+    }
